d1c38e66b3ab75ec89982b5fbb369e1eba10b9dd
Sharada <ltsharada@gmail.com>
2022-01-04 00:03:16 -0800

    Third commit with modified, routes.rb, schema.rb, seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+5.times do
+	Member.create!(
+		first_name: Faker::Name.first_name,
+		last_name:  Faker::Name.last_name,
+		url: Faker::Internet.url(host: 'example.com'),
+		email: Faker::Internet.email,
+		password: Faker::Internet.password(min_length: 8)
+	)
+end
